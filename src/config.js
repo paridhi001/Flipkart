8543d757a42312e16402ca6a9116c1bf4d55e1ca
Paridhi <paridhimaheshwari35@gmail.com>
2022-01-27 23:31:23 +0530

    little bit updation

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA5BZNlF1_6WBOil1ovpNkkK7xDD4TYCpM",
+    authDomain: "paridhi-flipkartclone.firebaseapp.com",
+    projectId: "paridhi-flipkartclone",
+    storageBucket: "paridhi-flipkartclone.appspot.com",
+    messagingSenderId: "11482229483",
+    appId: "1:11482229483:web:8242787fdf083be9fb6cae"
+  };
+
+export default firebaseConfig
